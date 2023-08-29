f28001824f75513cf45d1d4641f4c3a7ba526387
mnseok <min.seok@g.skku.edu>
2023-08-29 14:21:10 +0900

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -15,7 +15,7 @@ import { DndProvider } from "react-dnd";
 
 export default function Home() {
   const radius = 80;
-  const [bpm, setBPM] = useState<number>(10); // 초기값은 120
+  const [bpm, setBPM] = useState(120); // 초기값은 120
   const [n_beats, setn_beats] = useState(4); // 초기값은 4
   const [nth_partial, setnth_partial] = useState(4);
   const numButtons = n_beats * nth_partial;
@@ -23,52 +23,6 @@ export default function Home() {
   const instruments = ["kick", "snare-drum", "hi-hat", "low-floor-tom", "ride"];
   const [angle, setAngle] = useState<number>(0);
   const prevTimeRef: React.MutableRefObject<number | undefined> = useRef();
-
-  // useEffect(() => {
-  //   console.log("bpm: ", bpm, "typeof bpm: ", typeof bpm);
-  //   const animate = (time: number) => {
-  //     if (!prevTimeRef.current) {
-  //       prevTimeRef.current = time;
-  //     }
-  //     const deltaTime = time - prevTimeRef.current;
-  //     const angleDelta = ((bpm / 60) * 360 * (deltaTime / 1000)) % 360;
-  //     setAngle((prevAngle) => (prevAngle + angleDelta) % 360);
-
-  //     prevTimeRef.current = time;
-  //     requestAnimationFrame(animate);
-  //   };
-
-  //   const animationFrameId = requestAnimationFrame(animate);
-  //   return () => cancelAnimationFrame(animationFrameId);
-  // }, [bpm]);
-
-  // useEffect(() => {
-  //   const animate = (time: number) => {
-  //     if (!prevTimeRef.current) {
-  //       prevTimeRef.current = time;
-  //     }
-  //     const deltaTime = time - prevTimeRef.current;
-  //     const angleDelta = ((bpm / 60) * 360 * (deltaTime / 1000)) % 360;
-  //     console.log(
-  //       "angleDelta: ",
-  //       angleDelta,
-  //       "angle: ",
-  //       angle,
-  //       "bpm: ",
-  //       bpm,
-  //       "deltaTime: ",
-  //       deltaTime
-  //     );
-  //     setAngle((prevAngle) => (prevAngle + angleDelta) % 360);
-
-  //     prevTimeRef.current = time;
-  //     requestAnimationFrame(animate);
-  //   };
-
-  //   const animationFrameId = requestAnimationFrame(animate);
-  //   return () => cancelAnimationFrame(animationFrameId);
-  // }, [bpm]);
-
   const animateRef = useRef<FrameRequestCallback | null>(null);
 
   useEffect(() => {
